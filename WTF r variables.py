99594954bc7ddc6ddf8e03c07f4b9347b0c1d750
Louis Moench IV <49795953+nau5ea@users.noreply.github.com>
2020-05-23 14:35:41 -0700

    Add files via upload

---
+++ WTF r variables.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+a = 1
+
+
+# In[2]:
+
+
+print(a)
+
+
+# In[3]:
+
+
+b = "wtf"
+
+
+# In[4]:
+
+
+print(b)
+
+
+# In[30]:
+
+
+v1 = "first"
+v2 = "second"
+
+
+# In[31]:
+
+
+v3 = v1
+
+
+# In[32]:
+
+
+v1 = v2
+
+
+# In[33]:
+
+
+v2 = v3
+
+
+# In[34]:
+
+
+print(v2)
+print(v1)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
